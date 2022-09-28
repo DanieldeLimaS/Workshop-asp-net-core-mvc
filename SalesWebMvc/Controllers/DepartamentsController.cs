1fe775a7e7d38e6ada4f2ac11749325e4d3b0066
DanielLima <danielsuporteti@outlook.com.br>
2022-09-28 08:46:01 -0300

    First Model-controller-view - Departament

---
+++ SalesWebMvc/Controllers/DepartamentsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
+using System.Collections.Generic;
+
+namespace SalesWebMvc.Controllers
+{
+    public class DepartamentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Departament> list = new List<Departament>();
+            list.Add(new Departament { Id=1, Name="eletronic" });
+            list.Add(new Departament { Id=2, Name="eletronic2" });
+
+            return View(list);
+        }
+    }
+}

5631b76349eca138ef74cbe54f7633b1f7040f97
DanielLima <danielsuporteti@outlook.com.br>
2022-09-28 11:35:01 -0300

    Other entities and second migration

---
+++ SalesWebMvc/Models/SalesRecord.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SalesWebMvc.Models
+{
+    public class SalesRecord
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public double Amount { get; set; }
+        public ESaleStatus status { get; set; }
+        public Seller Seller { get; set; }
+        public SalesRecord()
+        {
+
+        }
+
+        public SalesRecord(int id, DateTime date, double amount, ESaleStatus status, Seller seller)
+        {
+            Id = id;
+            Date = date;
+            Amount = amount;
+            this.status = status;
+            Seller = seller;
+        }
+    }
+    public enum ESaleStatus : int
+    {
+        Pending=0,
+        Billed = 1,
+        Canceled = 2,
+    }
+}
